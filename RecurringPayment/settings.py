c04789d6cbaf8b79426756c986f96c247bd199de
Dev <saad.jamil@devsinc.com>
2022-08-03 11:26:58 +0500

    Added whitenoise

--- RecurringPayment/settings.py
+++ RecurringPayment/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'middlewares.permissions.PermissionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
